9d17b4e487aa938960f434990db076d8e25e41b9
Dracava <50298531+Dracava@users.noreply.github.com>
2023-05-19 15:47:47 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -9,20 +9,28 @@ let secondsColorB = 0;
 let drawingCanvas, saveImage;
 let drawingCommands = [];
 
-let timerStart = 9;
+let timerStart = 60;
 let timerLength = 1000;
-let timerCount = 9;
+let timerCount = 60;
 
-let sf = 1; // scaleFactor
-let x = 0; // pan X
-let y = 0; // pan Y
+let buttonX = 10;
+let buttonY = 10;
+
+let sf = 1;
+let x = 0;
+let y = 0;
 
 function setup() {
   drawingCanvas = createCanvas(windowWidth, windowHeight);
   background(255);
 
   //Timer
-  let timerValue = 9;
+  let timerValue = 10;
+
+  //Button clear
+  let clearButton = createButton("Clear");
+  clearButton.position(buttonX, buttonY);
+  clearButton.mousePressed(clearDrawing);
 }
 
 function draw() {
@@ -65,22 +73,20 @@ function draw() {
   }
 
   if (timerCount < 60 && timerCount > 30) {
-    //Button clear
-    let clearButton = createButton("Clear");
-    clearButton.position(10, 10);
-    clearButton.mousePressed(clearDrawing);
+  }
+  if (timerCount < 30) {
   }
   //Pensize changes after certain amount of time
   if (timerCount < 10) {
-    //penSize = 4;
+    penSize = 4;
     secondsColorR = 255;
   }
   //Offset changes after certain amount of time
   if (timerCount < 5) {
-    //(px = pmouseX - 30), (py = pmouseY), (x = mouseX - 30), (y = mouseY);
+    (px = pmouseX - 30), (py = pmouseY), (x = mouseX - 30), (y = mouseY);
   }
 
-  if (timerCount < 0 && timerCount > -3) {
+  if (timerCount < 0 && timerCount > -10) {
     strokeWeight(1);
     fill(255);
     rect(0, 0, width, 50);
@@ -88,52 +94,42 @@ function draw() {
     noStroke();
     text("Draw a pattern.", width / 2 - 50, 20);
     text("Time left:", width / 2 - 80, 40);
-    fill(255, 0, 0);
+    fill(secondsColorR, secondsColorG, secondsColorB);
     text("0", width / 2 - 10, 40);
     fill(0);
     text("seconds", width / 2 + 10, 40);
-
   }
-  if (timerCount <= -3) {
-  scale(sf);
-  translate(0, 0);
-  translate();
-  background(255);
-  for (let i = 0; i < drawingCommands.length; i++) {
-    let [px, py, x, y] = drawingCommands[i];
-    stroke(0);
-    strokeWeight(penSize);
-    line(px, py, x, y);
-    line(width*2 - x, y, width*2 - px, py);
-    line(x, height*2 - y, px, height*2 - py);
-    line(width*2 - x, height*2 - y, width*2 - px, height* 2 - py);
-  }
-  if (timerCount <= -4 && sf > 0.5) {
-    sf *= 0.995;
+  if (timerCount <= -10) {
+    scale(sf);
+    translate(0, 0);
+    translate();
+    background(255);
+    for (let i = 0; i < drawingCommands.length; i++) {
+      let [px, py, x, y] = drawingCommands[i];
+      stroke(0);
+      strokeWeight(penSize);
+      line(px, py, x, y);
+      line(width * 2 - x, y, width * 2 - px, py);
+      line(x, height * 2 - y, px, height * 2 - py);
+      line(width * 2 - x, height * 2 - y, width * 2 - px, height * 2 - py);
     }
-    else if(sf == 0.5){
-    //saveCanvas = drawingCanvas.get(0,50,width,height-50);
-    //saveCanvas.save('pattern', 'png');
-    }    
+    if (timerCount <= -11 && sf > 0.5) {
+      sf *= 0.995;
+    } else if (sf == 0.5) {
+      //saveCanvas = drawingCanvas.get(0,50,width,height-50);
+      //saveCanvas.save('pattern', 'png');
+    }
+  }
 }
 
 //Clear drawing
 function clearDrawing() {
   if (drawingCommands.length > 0) {
-    // Remove the last drawing command
-    let [px, py, x, y] = drawingCommands.pop();
-
     // Clear the canvas
     background(255);
+    drawingCommands.length = 0;
   }
+    //buttonX = width + 50;
+    //buttonY = height + 50;
+    //clearButton.position(buttonX, buttonY);
 }
-
-//Redo drawing
-function redoDrawing() {
-  translate(0, 0);
-  scale(sf);
-  translate(0, 0);
-  translate();
-  }
-}
-
