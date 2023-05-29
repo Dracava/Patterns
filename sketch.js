f21cf53a8024fc426ea1e64745cd1cdf20f0e88e
Dracava <50298531+Dracava@users.noreply.github.com>
2023-05-29 13:30:07 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -21,7 +21,7 @@ let x = 0;
 let y = 0;
 
 function setup() {
-  drawingCanvas = createCanvas(windowWidth, windowHeight);
+  drawingCanvas = createCanvas(windowWidth - 50, windowHeight);
   background(255);
 
   //Timer
